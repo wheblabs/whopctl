a0ad02c9c15517b5766b2f06419134103a18207c
ConstantineB6 <konstantinbelenko36@gmail.com>
2025-10-23 15:30:21 -0700

    supposed to work now

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+import chalk from 'chalk'
+
+async function main() {
+	console.log('Hello, world!')
+}
+
+main().catch((err) => {
+	console.error(chalk.red('Error:'), err)
+	process.exit(1)
+})
